8354b37602571a4605ec5839b3b2543c086e7463
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-07-17 20:54:42 -0300

    Update matrix_effect.bat

--- matrix_effect.bat
+++ matrix_effect.bat
@@ -1,6 +1,5 @@
 @echo off
 color 2
  :start
-echo %random% %random% %random% %random% %random%
-%random% %random%%random% %random% %random%.
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
 goto start
